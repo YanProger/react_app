f01d0cd338e20e7bc7833a9bace80746d835ddea
Yan <losevyan@gmail.com>
2017-11-15 14:20:57 +0500

    sad

--- com-app/src/index.js
+++ com-app/src/index.js
@@ -2,25 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class ShoppingList extends React.Component {
-    render() {
+class Row extends React.Component {
+    render(){
         return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                    <li>Oculussdasdsa</li>
-                </ul>
+            <div className="row">
+                {<Cell cellValue="val1"/>}
+                {<Cell cellValue="val2"/>}
             </div>
         );
     }
 }
 
+class Cell extends React.Component {
+    render(){
+        return (
+            <div className="cell">
+                {this.props.cellValue}
+            </div>
+
+        );
+    }
+}
+
+// class Table extends React.Component {
+//     render() {
+//         return (
+//             <div className="shopping-list">
+//                 <h1>Shopping List for {this.props.name}</h1>
+//                 <ul>
+//                     <li>Instagram</li>
+//                     <li>WhatsApp</li>
+//                     <li>Oculus</li>
+//                     <li>Oculussdasdsa</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
 //--------------------------------
 
 ReactDOM.render(
-  <ShoppingList/>,
+  <Row cellCount={9}/>,
     document.getElementById('root')
 );

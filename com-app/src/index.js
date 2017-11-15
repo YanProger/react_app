7e10bd42f1654c4ae961d740f6c40234580308ca
LosevYan <losev@utip.ru>
2017-11-15 19:50:04 +0500

    made table

--- com-app/src/index.js
+++ com-app/src/index.js
@@ -6,8 +6,7 @@ class Row extends React.Component {
     render(){
         return (
             <div className="row">
-                {<Cell cellValue="val1"/>}
-                {<Cell cellValue="val2"/>}
+                {this.props.row.map((e, index) => <Cell key={index} value={e}/>)}
             </div>
         );
     }
@@ -17,32 +16,52 @@ class Cell extends React.Component {
     render(){
         return (
             <div className="cell">
-                {this.props.cellValue}
+                {this.props.value}
             </div>
 
         );
     }
 }
 
-// class Table extends React.Component {
-//     render() {
-//         return (
-//             <div className="shopping-list">
-//                 <h1>Shopping List for {this.props.name}</h1>
-//                 <ul>
-//                     <li>Instagram</li>
-//                     <li>WhatsApp</li>
-//                     <li>Oculus</li>
-//                     <li>Oculussdasdsa</li>
-//                 </ul>
-//             </div>
-//         );
-//     }
-// }
+class Table extends React.Component {
+    render(){
+        return (
+            <div className="table">
+                {this.props.table.map((e, index) => <Row key={index} row={e}/>)}
+            </div>
+        );
+    }
+}
+
+let table = [];
+
+let row1 = [];
+row1.push("Fist");
+row1.push("Second");
+row1.push("Third");
+row1.push("Four");
+
+let row2 = [];
+row2.push("Fist");
+row2.push("Second");
+row2.push("Third");
+row2.push("Four");
+
+let row3 = [];
+row3.push("Fist");
+row3.push("Second");
+row3.push("Third");
+row3.push("Four");
+
+table.push(row1);
+table.push(row2);
+table.push(row3);
+
 
 //--------------------------------
 
+
 ReactDOM.render(
-  <Row cellCount={9}/>,
+  <Table table={table}/>,
     document.getElementById('root')
 );
